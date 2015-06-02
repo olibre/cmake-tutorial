e7cf72b5fadea2c68d22ee990ac481ba94bd8cf9
jinwei <jinwei@sensetime.com>
2015-06-02 23:59:39 +0800

    tradingsys

---
+++ cmake_tut/tradingsys/build/CMakeFiles/trading_sys.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/trading_sys.dir/trading_sys.cpp.o"
+  "CMakeFiles/trading_sys.dir/parser.cpp.o"
+  "trading_sys.pdb"
+  "trading_sys"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/trading_sys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
